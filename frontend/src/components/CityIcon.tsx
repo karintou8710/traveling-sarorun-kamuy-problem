7b29adfd636bee66db036b6c749c2edca017cfdc
Masahito Kumada <5445197440@edu.k.u-tokyo.ac.jp>
2021-03-20 15:48:47 +0900

    set figure margin on front

--- frontend/src/components/CityIcon.tsx
+++ frontend/src/components/CityIcon.tsx
@@ -10,28 +10,22 @@ interface Props {
 function CityIcon(props: Props) {
 
   const {city_name} = props;
-  
   return (
     <div>
         {city_name === "小樽" ? <img src={require('./assets/otaru.png').default} width="300" height="auto"/> : null}
-        
         {city_name === "札幌" ? <img src={require('./assets/sapporo.jpg').default} width="300" height="auto" /> : null}
-
         {city_name === "室蘭" ? <img src={require('./assets/muroran.jpg').default} width="300" height="auto" /> : null}
-
         {city_name === "知床" ? <img src={require('./assets/shiretoko.jpg').default} width="300" height="auto"/> : null}
-
         {city_name === "旭川" ? <img src={require('./assets/asahikawa.jpg').default} width="300" height="auto" /> : null}
     </div>
-    
-
   );
-  
-
 }
 
-
-
-
 export default CityIcon;
+
+
+const A  = styled.div`
+margin: 0px;
+
+`
 
