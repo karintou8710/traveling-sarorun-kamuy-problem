9462e632854807f5de65d12932236bb8759b7b61
karintou8710 <karintou8710@gmail.com>
2021-03-20 11:44:35 +0900

    [fix] bitdpのtimes修正

--- backend/tspAPI/module/bitdp.py
+++ backend/tspAPI/module/bitdp.py
@@ -77,8 +77,13 @@ class BitDP:
             self.times.append(self.graph[prevVertex][v])
             v = prevVertex
         
+        self.times.append(0)
+        
         self.route.reverse()
         self.times.reverse()
+
+        for i in range(len(self.times)-1):
+            self.times[i+1] += self.times[i]
 
     def getValue(self):
         """
